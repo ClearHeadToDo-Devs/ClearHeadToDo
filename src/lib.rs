08b70b11e2be06dfd1d5d5f5dd69c0c0f9565883
tebding <tyler.ebding@gmail.com>
2020-08-07 17:02:53 -0700

    implemented mark_completed(), test passes

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,18 @@
 pub struct Task {
-    name: String
+    name: String,
+    completed: bool,
 }
 
 pub fn create_task(task_name: String) -> Task {
     Task {
-        name: String::from("Test Task")
+        name: String::from("Test Task"),
+        completed: false,
+    }
+}
+
+impl Task {
+    pub fn mark_complete(&mut self) {
+        self.completed = true;
     }
 }
 
@@ -16,5 +24,12 @@ mod tests {
     fn task_creation() {
         let TestTask = create_task(String::from("Test Task"));
         assert!(TestTask.name == "Test Task")
+    }
+
+    #[test]
+    fn task_completion() {
+        let mut TestTask = create_task(String::from("Test Task"));
+        TestTask.mark_complete();
+        assert!(TestTask.completed == true);
     }
 }
