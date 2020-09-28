37a085a45f4223d7f7c9624fcdaeced80e583c61
Darrion <dargondab9@gmail.com>
2020-09-27 23:07:55 -0500

    added some more CLI tests and changed them to be more inclusive of none answers

--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,17 @@ pub struct CLI{
 
 impl CLI {
         pub fn parse_arguments(&mut self) {
-            match self.pattern.as_ref().unwrap() as &str{
-            "create_task" | "create" | "ct" | "new_task" | "new" => self.task_vec
+            match self.pattern.as_ref().unwrap_or(&"no command given".to_string()) as &str{
+            "create_task" | "create" | "ct" | "new_task" | "new" =>
+                self.task_vec
                 .create_task(),
-            "list_tasks" | "lt" | "list" | "list_all" => self.task_vec
+            "list_tasks" | "lt" | "list" | "list_all" =>
+                self.task_vec
                 .print_task_list(
                     io::stdout())
-                    .unwrap(),
-            "remove_task" | "remove" | "rt" | "delete_task" | "delete" => self.task_vec
+                    .unwrap_or(()),
+            "remove_task" | "remove" | "rt" | "delete_task" | "delete" =>
+                self.task_vec
                 .remove_task(
                     self.index.as_ref()
                     .unwrap()
@@ -29,20 +32,23 @@ impl CLI {
                     .unwrap(), 
                     io::stdout())
                     .expect("invalid index"),
-            "complete_task" | "complete" | "mark_complete" => self.task_vec.tasks[
+            "complete_task" | "complete" | "mark_complete" =>
+                self.task_vec.tasks[
                 self.index.as_ref()
                 .unwrap()
                 .parse::<usize>()
                 .unwrap()]
                 .mark_complete(),
-            "change_priority" | "cp" | "new_priority" | "np" => self.task_vec.tasks[
+            "change_priority" | "cp" | "new_priority" | "np" =>
+                self.task_vec.tasks[
                 self.index.as_ref()
                 .unwrap()
                 .parse::<usize>()
                 .unwrap()]
                 .change_priority(
                         &self.input.as_ref().unwrap()[..]),
-            "rename_task" | "rename" | "name" | "r" => self.task_vec.tasks[
+            "rename_task" | "rename" | "name" | "r" =>
+                self.task_vec.tasks[
                 self.index.as_ref()
                .unwrap()
                .parse::<usize>()
@@ -81,21 +87,38 @@ mod tests {
 
     #[test]
     fn cli_creation_test () {
-        let test_cli = CLI {
-            pattern: Some("test_pattern".to_string()), 
-            index: Some("test_index".to_string()),
-            input: Some("test_input".to_string()),
+        let mut test_cli = CLI {
+            pattern: None, 
+            index: None,
+            input: None,
             task_vec: TaskList{tasks: vec![]}, 
 
         };
-        assert!(test_cli.pattern == Some("test_pattern".to_string()));
-        assert!(test_cli.index == Some("test_index".to_string()));
-        assert!(test_cli.input == Some("test_input".to_string()));
+        assert!(test_cli.pattern == None);
+        assert!(test_cli.index == None);
+        assert!(test_cli.input == None);
         assert!(test_cli.task_vec.tasks.len() == 0);
+        test_cli.parse_arguments();
     }
 
     #[test]
     fn cli_task_creation_test () {
+        let mut test_cli = CLI {
+            pattern: Some("create_task".to_string()), 
+            index: None,
+            input: None,
+            task_vec: TaskList{tasks: vec![]}, 
+
+        };
+        test_cli.parse_arguments();
+        assert!(test_cli.task_vec.tasks.len() == 1);
+        assert!(test_cli.task_vec.tasks[0].name == "Test Task");
+        assert!(test_cli.task_vec.tasks[0].completed == false);
+        assert!(test_cli.task_vec.tasks[0].priority == PriEnum::Optional);
+    }
+
+    #[test]
+    fn cli_task_list_test () {
         let mut test_cli = CLI {
             pattern: Some("create_task".to_string()), 
             index: None,
