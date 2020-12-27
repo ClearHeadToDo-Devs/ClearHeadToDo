adddf214327d2bac3496add6d1313b5367ff969a
Darrion <dargondab9@gmail.com>
2020-12-26 20:46:34 -0600

    main now has the index as a usize, this way, all the logic can be done once and that will cut down on how chunky parse_arguments was

--- src/main.rs
+++ src/main.rs
@@ -5,56 +5,51 @@ use std::io::stdout;
 use std::io::{self, Write};
 use std::path::Path;
 
-pub struct CLI {
+pub struct Cli {
     pub pattern: Option<String>,
-    pub index: Option<String>,
+    pub index: Option<usize>,
     pub input: Option<String>,
     pub task_vec: TaskList,
 }
 
-impl CLI {
+impl Cli {
     pub fn parse_arguments(&mut self) -> Result<String, Box<dyn Error>> {
-        match self
-            .pattern
-            .as_ref()
-            .unwrap_or(&"no command given".to_string()) as &str
-        {
+        match self.pattern.as_ref().unwrap() as &str {
             "create_task" | "create" | "ct" | "new_task" | "new" => self.task_vec.create_task(),
             "list_tasks" | "lt" | "list" | "list_all" => {
                 self.task_vec.print_task_list(std::io::stdout())
             }
             "remove_task" | "remove" | "rt" | "delete_task" | "delete" => {
-                self.task_vec.remove_task(
-                    self.index
-                        .as_ref()
-                        .unwrap()
-                        .to_string()
-                        .parse::<usize>()
-                        .unwrap(),
-                )
+                self.task_vec.remove_task(self.index.unwrap())
             }
-            "complete_task" | "complete" | "mark_complete" => self.task_vec.tasks
-                [self.index.as_ref().unwrap().parse::<usize>().unwrap()]
-            .mark_complete(),
+            "complete_task" | "complete" | "mark_complete" => {
+                self.task_vec.tasks[self.index.unwrap()].mark_complete()
+            }
             "change_priority" | "cp" | "new_priority" | "np" => self.task_vec.tasks
-                [self.index.as_ref().unwrap().parse::<usize>().unwrap()]
+                [self.index.unwrap()]
             .change_priority(&self.input.as_ref().unwrap()[..]),
-            "rename_task" | "rename" | "name" | "r" => self.task_vec.tasks
-                [self.index.as_ref().unwrap().parse::<usize>().unwrap()]
-            .rename_task(self.input.as_ref().unwrap()),
+            "rename_task" | "rename" | "name" | "r" => {
+                self.task_vec.tasks[self.index.unwrap()].rename_task(self.input.as_ref().unwrap())
+            }
             _ => return Ok("Try putting in a command to see what we can do!".to_string()),
         }
     }
-    // pub fn cli_list_tasks(&self, mut writer: impl std::io::Write) {
-    //     self.task_vec.print_task_list(writer).unwrap_or(());
-    // }
 }
+
 fn main() {
     println!("starting program");
 
-    let mut main_cli: CLI = CLI {
+    let mut main_cli: Cli = Cli {
         pattern: std::env::args().nth(1),
-        index: std::env::args().nth(2),
+        index: Some(
+            std::env::args()
+                .nth(2)
+                .as_ref()
+                .unwrap()
+                .to_string()
+                .parse::<usize>()
+                .unwrap(),
+        ),
         input: std::env::args().nth(3),
         task_vec: TaskList { tasks: vec![] },
     };
@@ -62,8 +57,8 @@ fn main() {
     main_cli.task_vec.load_tasks("tasks.csv").unwrap();
 
     match main_cli.parse_arguments() {
-        Ok(Response) => println!("{}",Response),
-        Err(Error) => println!("{}",Error)
+        Ok(output) => println!("{}", output),
+        Err(error) => println!("{}", error),
     };
 
     main_cli.task_vec.load_csv("tasks.csv").unwrap();
@@ -75,7 +70,7 @@ mod tests {
 
     #[test]
     fn cli_creation_test() {
-        let mut test_cli = CLI {
+        let mut test_cli = Cli {
             pattern: None,
             index: None,
             input: None,
@@ -85,18 +80,17 @@ mod tests {
         assert!(test_cli.index == None);
         assert!(test_cli.input == None);
         assert!(test_cli.task_vec.tasks.len() == 0);
-        test_cli.parse_arguments();
     }
 
     #[test]
     fn cli_task_creation_test() {
-        let mut test_cli = CLI {
+        let mut test_cli = Cli {
             pattern: Some("create_task".to_string()),
             index: None,
             input: None,
             task_vec: TaskList { tasks: vec![] },
         };
-        test_cli.parse_arguments();
+        test_cli.parse_arguments().unwrap();
         assert!(test_cli.task_vec.tasks.len() == 1);
         assert!(test_cli.task_vec.tasks[0].name == "Test Task");
         assert!(test_cli.task_vec.tasks[0].completed == false);
@@ -106,13 +100,14 @@ mod tests {
     #[test]
     fn cli_task_list_test() {
         //let mut good_result = Vec::new();
-        let mut test_cli = CLI {
+        let mut test_cli = Cli {
             pattern: Some("list_tasks".to_string()),
             index: None,
             input: None,
             task_vec: TaskList { tasks: vec![] },
         };
-
-        test_cli.parse_arguments();
+        test_cli.task_vec.create_task().unwrap();
+        let response: String = test_cli.parse_arguments().unwrap();
+        assert_eq!(response, "Successfully Printed {}");
     }
 }
