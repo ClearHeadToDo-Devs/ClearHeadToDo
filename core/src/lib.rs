b0c17507a6f7256c6f2c5eb1021efa6e89f775a8
Darrion Burgess <dargondab9@gmail.com>
2021-10-19 22:42:22 -0500

    just need to save these small changes I made before I actually remove the task list struct

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -7,6 +7,9 @@ pub use helper::*;
 pub mod storage;
 pub use storage::*;
 
+pub mod task_list_manipulation;
+pub use task_list_manipulation::TaskListManipulation;
+
 pub mod command;
 pub use command::*;
 
@@ -18,30 +21,6 @@ use uuid::Uuid;
 #[derive(Debug, PartialEq, Clone)]
 pub struct TaskList {
     pub tasks: im::Vector<Task>,
-}
-
-pub trait TaskListManipulation {
-    type Child;
-    fn create_task_list() -> Self;
-    fn create_task(&self) -> Self;
-    fn print_task_list(&self) -> Result<String, Box<dyn Error>>;
-    fn remove_task(&self, index: usize) -> Result<Self, Box<dyn Error>>
-    where
-        Self: Sized;
-    fn rename_task(&self, index: usize, new_name: String) -> Result<Self, Box<dyn Error>>
-    where
-        Self: Sized;
-    fn toggle_task_completion_status(&self, index: usize) -> Result<Self, Box<dyn Error>>
-    where
-        Self: Sized;
-    fn change_task_priority(
-        &self,
-        index: usize,
-        new_priority: String,
-    ) -> Result<Self, Box<dyn Error>>
-    where
-        Self: Sized;
-    fn select_task_by_id(&self, id: Uuid) -> Result<Self::Child, Box<dyn Error>>;
 }
 
 impl TaskListManipulation for TaskList {
