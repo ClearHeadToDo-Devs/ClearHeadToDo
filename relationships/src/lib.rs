91c5689eff8882faae872b693a4a1bc0a92c32f2
Darrion Burgess <dargondab9@gmail.com>
2022-03-20 18:57:12 -0500

    refactored to make the tests better for ergonomics

--- relationships/src/lib.rs
+++ relationships/src/lib.rs
@@ -67,41 +67,27 @@ mod tests {
         );
 
         assert!(
-            relationship.direction == EdgeDirection::Directed
-                && relationship.participant_1 == first_participant_id
-                && relationship.participant_2 == second_participant_id
+                relationship.participant_2 != relationship.participant_1
         )
     }
 
     #[test]
     fn two_successful_relationship_creations() {
-        let first_participant_id = Uuid::new_v4();
-        let second_participant_id = Uuid::new_v4();
-        let third_participant_id = Uuid::new_v4();
-
-        let direction_1 = EdgeDirection::Directed;
-        let direction_2 = EdgeDirection::Undirected;
+        let nil_participant_id = Uuid::nil();
 
         let relationship_1 = Relationship::create_new(
-            direction_1,
-            first_participant_id,
-            second_participant_id,
+            EdgeDirection::Directed,
+            nil_participant_id,
+            nil_participant_id,
         );
         let relationship_2 = Relationship::create_new(
-            direction_2,
-            first_participant_id,
-            third_participant_id,
+            EdgeDirection::Directed,
+            nil_participant_id,
+            nil_participant_id,
         );
 
         assert!(
-            relationship_1.direction == EdgeDirection::Directed
-                && relationship_1.participant_1 == first_participant_id
-                && relationship_1.participant_2 == second_participant_id
-        );
-        assert!(
-            relationship_2.direction == EdgeDirection::Undirected
-                && relationship_2.participant_1 == first_participant_id
-                && relationship_2.participant_2 == third_participant_id
+                relationship_2.id != relationship_1.id
         );
     }
 }
