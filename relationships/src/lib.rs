4ed05d61d68384af4a94f15b57df79c1ffff80c3
Darrion Burgess <dargondab9@gmail.com>
2022-03-19 20:22:04 -0500

    made a struct for the relationship types

--- relationships/src/lib.rs
+++ relationships/src/lib.rs
@@ -1,6 +1,24 @@
-#[allow(dead_code)]
-fn create_relationship(_participant_1: &str, _participant_2: &str) -> Vec<String>{
-    return vec!["first".to_string(), "second".to_string()]
+#[derive(PartialEq)]
+enum RelationshipType {
+    ParentChild,
+    PreviousSubsequent,
+    Related,
+}
+struct Relationship {
+    relationship_type: RelationshipType,
+    participant_1: usize,
+    participant_2: usize
+}
+
+impl Relationship{
+    #[allow(dead_code)]
+    fn create_relationship(relationship_type: RelationshipType, participant_1: usize, participant_2: usize) -> Self{
+        return Relationship{
+            relationship_type: relationship_type,
+            participant_1, 
+            participant_2
+        }
+    }
 }
 #[cfg(test)]
 mod tests {
@@ -8,9 +26,28 @@ mod tests {
 
     #[test]
     fn successful_relationship_creation() {
-        let first_participant = "first";
-        let second_participant = "second";
-        let relationship = create_relationship(first_participant, second_participant);
-        assert!(relationship[0]==first_participant && relationship[1]==second_participant)
+        let first_participant_id = 1;
+        let second_participant_id = 2;
+        let relationship_type = RelationshipType::ParentChild;
+
+        let relationship = Relationship::create_relationship(relationship_type, first_participant_id, second_participant_id);
+
+        assert!(relationship.relationship_type==RelationshipType::ParentChild && relationship.participant_1==1 && relationship.participant_2==2)
+    }
+
+    #[test]
+    fn two_successful_relationship_creations(){
+        let first_participant_id = 1;
+        let second_participant_id = 2;
+        let third_participant_id = 3;
+
+        let relationship_type_1 = RelationshipType::ParentChild;
+        let relationship_type_2 = RelationshipType::PreviousSubsequent;
+
+        let relationship_1 = Relationship::create_relationship(relationship_type_1, first_participant_id, second_participant_id);
+        let relationship_2 = Relationship::create_relationship(relationship_type_2, first_participant_id, third_participant_id);
+
+        assert!(relationship_1.relationship_type==RelationshipType::ParentChild && relationship_1.participant_1==1 && relationship_1.participant_2==2);
+        assert!(relationship_2.relationship_type==RelationshipType::PreviousSubsequent && relationship_2.participant_1==1 && relationship_2.participant_2==3);
     }
 }
