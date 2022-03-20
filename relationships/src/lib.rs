01df1e49bd55937db2e6115fa9f49ee6e6f3884d
Darrion Burgess <dargondab9@gmail.com>
2022-03-20 08:55:52 -0500

    formatting the main file

--- relationships/src/lib.rs
+++ relationships/src/lib.rs
@@ -4,20 +4,25 @@ enum RelationshipType {
     PreviousSubsequent,
     Related,
 }
+#[allow(dead_code)]
 struct Relationship {
     relationship_type: RelationshipType,
     participant_1: usize,
-    participant_2: usize
+    participant_2: usize,
 }
 
-impl Relationship{
+impl Relationship {
     #[allow(dead_code)]
-    fn create_relationship(relationship_type: RelationshipType, participant_1: usize, participant_2: usize) -> Self{
-        return Relationship{
+    fn create_relationship(
+        relationship_type: RelationshipType,
+        participant_1: usize,
+        participant_2: usize,
+    ) -> Self {
+        return Relationship {
             relationship_type: relationship_type,
-            participant_1, 
-            participant_2
-        }
+            participant_1,
+            participant_2,
+        };
     }
 }
 #[cfg(test)]
@@ -30,13 +35,21 @@ mod tests {
         let second_participant_id = 2;
         let relationship_type = RelationshipType::ParentChild;
 
-        let relationship = Relationship::create_relationship(relationship_type, first_participant_id, second_participant_id);
+        let relationship = Relationship::create_relationship(
+            relationship_type,
+            first_participant_id,
+            second_participant_id,
+        );
 
-        assert!(relationship.relationship_type==RelationshipType::ParentChild && relationship.participant_1==1 && relationship.participant_2==2)
+        assert!(
+            relationship.relationship_type == RelationshipType::ParentChild
+                && relationship.participant_1 == 1
+                && relationship.participant_2 == 2
+        )
     }
 
     #[test]
-    fn two_successful_relationship_creations(){
+    fn two_successful_relationship_creations() {
         let first_participant_id = 1;
         let second_participant_id = 2;
         let third_participant_id = 3;
@@ -44,10 +57,26 @@ mod tests {
         let relationship_type_1 = RelationshipType::ParentChild;
         let relationship_type_2 = RelationshipType::PreviousSubsequent;
 
-        let relationship_1 = Relationship::create_relationship(relationship_type_1, first_participant_id, second_participant_id);
-        let relationship_2 = Relationship::create_relationship(relationship_type_2, first_participant_id, third_participant_id);
+        let relationship_1 = Relationship::create_relationship(
+            relationship_type_1,
+            first_participant_id,
+            second_participant_id,
+        );
+        let relationship_2 = Relationship::create_relationship(
+            relationship_type_2,
+            first_participant_id,
+            third_participant_id,
+        );
 
-        assert!(relationship_1.relationship_type==RelationshipType::ParentChild && relationship_1.participant_1==1 && relationship_1.participant_2==2);
-        assert!(relationship_2.relationship_type==RelationshipType::PreviousSubsequent && relationship_2.participant_1==1 && relationship_2.participant_2==3);
+        assert!(
+            relationship_1.relationship_type == RelationshipType::ParentChild
+                && relationship_1.participant_1 == 1
+                && relationship_1.participant_2 == 2
+        );
+        assert!(
+            relationship_2.relationship_type == RelationshipType::PreviousSubsequent
+                && relationship_2.participant_1 == 1
+                && relationship_2.participant_2 == 3
+        );
     }
 }
