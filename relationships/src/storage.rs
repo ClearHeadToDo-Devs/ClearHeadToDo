e21cf3e97196a0ddca801a1463523310ed49e147
Darrion Burgess <dargondab9@gmail.com>
2022-07-31 21:45:15 -0500

    now i'm able to test properly for this work

--- relationships/src/storage.rs
+++ relationships/src/storage.rs
@@ -1,5 +1,4 @@
-use serde::{Deserialize, Serialize};
-use std::{error::Error, fs::File, io::Write, path::Path};
+use std::{error::Error, path::Path};
 
 use crate::Relationship;
 use im::Vector;
@@ -23,10 +22,10 @@ mod test {
     use crate::tests::add_nil_relationship_to_vector;
 
     #[test]
-    fn load_relationship_list() {
+    fn successful_relationship_list_save() {
         let empty_list: Vector<Relationship> = Vector::new();
         let single_list = add_nil_relationship_to_vector(empty_list);
-        let file_path = Path::new("../data/test_relationship.csv");
+        let file_path = Path::new("data/test_relationship.csv");
         let mut file_contents = String::new();
 
         single_list.write_to_csv(file_path).unwrap();
@@ -36,5 +35,19 @@ mod test {
             .read_to_string(&mut file_contents)
             .unwrap();
         assert_eq!(file_contents,"id,variant,participant_1,participant_2\n00000000-0000-0000-0000-000000000000,Undirected,00000000-0000-0000-0000-000000000000,00000000-0000-0000-0000-000000000000\n")
+    }
+
+    #[test]
+    fn failed_relationship_list_save() {
+        let empty_list: Vector<Relationship> = Vector::new();
+        let single_list = add_nil_relationship_to_vector(empty_list);
+        let file_path = Path::new("bad_folder/bad_file");
+
+        let directory_error = single_list.write_to_csv(file_path).unwrap_err();
+
+        assert_eq!(
+            directory_error.to_string(),
+            "No such file or directory (os error 2)"
+        )
     }
 }
