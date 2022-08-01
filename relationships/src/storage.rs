79c9a8f7f6c047076f1d1391b1c2416ce7b280b8
Darrion Burgess <dargondab9@gmail.com>
2022-07-31 21:30:51 -0500

    did it, I was finally able to implement a serialization format that is able to actually handle the file writing process easily

--- relationships/src/storage.rs
+++ relationships/src/storage.rs
@@ -1,11 +1,18 @@
+use serde::{Deserialize, Serialize};
+use std::{error::Error, fs::File, io::Write, path::Path};
+
 use crate::Relationship;
 use im::Vector;
 pub trait CsvStorage {
-    fn write_to_csv(&self) -> ();
+    fn write_to_csv(&self, file_path: &Path) -> Result<(), Box<dyn Error>>;
 }
 
 impl CsvStorage for Vector<Relationship> {
-    fn write_to_csv(&self) -> () {}
+    fn write_to_csv(&self, file_path: &Path) -> Result<(), Box<dyn Error>> {
+        let mut file = csv::Writer::from_path(file_path)?;
+        file.serialize(self)?;
+        Ok(())
+    }
 }
 
 #[cfg(test)]
@@ -22,12 +29,12 @@ mod test {
         let file_path = Path::new("../data/test_relationship.csv");
         let mut file_contents = String::new();
 
-        single_list.write_to_csv();
+        single_list.write_to_csv(file_path).unwrap();
 
         fs::File::open(file_path)
             .unwrap()
             .read_to_string(&mut file_contents)
             .unwrap();
-        assert!(file_contents == "")
+        assert_eq!(file_contents,"id,variant,participant_1,participant_2\n00000000-0000-0000-0000-000000000000,Undirected,00000000-0000-0000-0000-000000000000,00000000-0000-0000-0000-000000000000\n")
     }
 }
