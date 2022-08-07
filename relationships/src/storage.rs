d69d394cf27241363a8ac7a853807c348db6b948
Darrion Burgess <dargondab9@gmail.com>
2022-08-06 23:27:14 -0500

    ugh still so annoying, I have what I think is the right code to serialize to JSON, but I have been having trouble getting the thing to actually write despite running into no specific error codes

--- relationships/src/storage.rs
+++ relationships/src/storage.rs
@@ -1,15 +1,21 @@
 use crate::Relationship;
 use crate::RelationshipListManagement;
 
+use std::fs::File;
+use std::io::BufWriter;
 use std::{error::Error, path::Path};
 
 use im::Vector;
-use serde::Deserialize;
 
 pub trait CsvStorage {
     type L: RelationshipListManagement;
     fn write_to_csv(&self, file_path: &Path) -> Result<(), Box<dyn Error>>;
     fn read_from_csv(file_path: &Path) -> Result<Self::L, Box<dyn Error>>;
+}
+
+pub trait JSONStorage {
+    type L: RelationshipListManagement;
+    fn write_to_json(&self, file_path: &Path) -> Result<(), Box<dyn Error>>;
 }
 
 impl CsvStorage for Vector<Relationship> {
@@ -29,6 +35,18 @@ impl CsvStorage for Vector<Relationship> {
         }
 
         Ok(relationship_list)
+    }
+}
+
+impl JSONStorage for Vector<Relationship> {
+    type L = Vector<Relationship>;
+    fn write_to_json(&self, file_path: &Path) -> Result<(), Box<dyn Error>> {
+        let file = File::open(file_path)?;
+        let file_writer = BufWriter::new(file);
+
+        serde_json::to_writer(file_writer, &self)?;
+
+        Ok(())
     }
 }
 
@@ -71,8 +89,26 @@ mod test {
     }
     #[test]
     fn successful_relationship_list_read() {
-        let file_path = Path::new("data/successful_relationship_list_read_test.csv");
+        //Need to be sure and implement or delete this after the JSON variant is completed
+        //let file_path = Path::new("data/successful_relationship_list_read_test.csv");
 
-        let relationship_list: Vector<Relationship> = Vector::read_from_csv(file_path).unwrap();
+        //let relationship_list: Vector<Relationship> = Vector::read_from_csv(file_path).unwrap();
+    }
+
+    #[test]
+    fn successfully_read_json() {
+        let empty_list: Vector<Relationship> = Vector::new();
+        let single_list = add_nil_relationship_to_vector(empty_list);
+        let file_path = Path::new("data/test_relationship.json");
+        let mut file_contents = String::new();
+
+        single_list.write_to_json(file_path).unwrap();
+
+        fs::File::open(file_path)
+            .unwrap()
+            .read_to_string(&mut file_contents)
+            .unwrap();
+
+        assert_eq!(file_contents,"id,variant,participant_1,participant_2\n00000000-0000-0000-0000-000000000000,Undirected,00000000-0000-0000-0000-000000000000,00000000-0000-0000-0000-000000000000\n")
     }
 }
