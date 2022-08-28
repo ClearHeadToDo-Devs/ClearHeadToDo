33e00250438e00c88a068098f51e9422618f5f2c
Darrion Burgess <dargondab9@gmail.com>
2022-08-27 20:35:41 -0500

    so far, so good, was able to move bulk of task lib to another file

--- task/src/lib.rs
+++ task/src/lib.rs
@@ -7,68 +7,6 @@ pub use task_manipulation::TaskManipulation;
 pub mod storage;
 pub use storage::*;
 
-use std::error::Error;
-use uuid::Uuid;
-
-use serde::Deserialize;
-
 pub mod item;
 pub use item::*;
 
-#[derive(PartialEq, Debug, Clone, Deserialize)]
-pub struct Task {
-    pub id: Uuid,
-    pub name: String,
-    pub completed: bool,
-    pub priority: PriEnum,
-}
-
-impl Default for Task {
-    fn default() -> Task {
-        Task {
-            id: Uuid::new_v4(),
-            name: "Default Task".to_string(),
-            completed: false,
-            priority: Default::default(),
-        }
-    }
-}
-
-impl TaskManipulation for Task {
-    fn rename(&self, new_task_name: &str) -> Task {
-        return Task {
-            name: new_task_name.to_owned(),
-            ..self.to_owned()
-        };
-    }
-    fn toggle_completion_status(&self) -> Task {
-        Task {
-            completed: !self.completed,
-            ..self.to_owned()
-        }
-    }
-
-    fn change_priority(&self, new_priority: &str) -> Result<Task, Box<dyn Error>> {
-        let new_pri: PriEnum = PriEnum::parse_priority(new_priority)?;
-        return Ok(Task {
-            priority: new_pri.clone(),
-            ..self.to_owned()
-        });
-    }
-
-    fn export_fields_as_string(&self) -> String {
-        format!(
-            "{name},{priority},{completed},{ID}\n",
-            name = self.name,
-            priority = self.priority.to_string(),
-            completed = self.completed,
-            ID = self.id
-        )
-    }
-
-    fn create_default_task() -> Task {
-        Task {
-            ..Default::default()
-        }
-    }
-}
