4f08b8faffaf2ee9562c8b95e5b36614d745578b
Darrion Burgess <dargondab9@gmail.com>
2022-08-29 22:52:11 -0500

    got the test working by ensuring the trait bounds are implemented properly

--- core/src/functionality.rs
+++ core/src/functionality.rs
@@ -3,11 +3,25 @@ use relationships::RelationshipListManagement;
 
 use std::fmt::Debug;
 use std::cmp::PartialEq;
+use std::error::Error;
 
-#[derive(Debug, PartialEq, Default)]
-struct ClearHeadApp<A: ActionListManipulation, R: RelationshipListManagement>  {
+#[derive(Debug, PartialEq, Default, Clone)]
+struct ClearHeadApp<A: ActionListManipulation + Clone, R: RelationshipListManagement + Clone>  {
     action_list: A,
     relationship_list: R,
+}
+
+impl <A: ActionListManipulation + Clone,R: RelationshipListManagement + Clone>ClearHeadApp<A, R> {
+    fn create(&self) -> Result<ClearHeadApp<A, R>, Box<dyn Error>>  {
+        let cloned_app = self.clone();
+        let new_action_list = self.action_list.create_new();
+
+        Ok(ClearHeadApp {
+            action_list: new_action_list,
+            relationship_list: cloned_app.relationship_list,
+        })
+    }
+
 }
 
 #[cfg(test)]
@@ -22,5 +36,13 @@ mod tests {
         let test_app: ClearHeadApp<Vector<Action>, Vector<Relationship>> = Default::default();
         assert_eq!(test_app.action_list, Vector::new());
         assert_eq!(test_app.relationship_list, Vector::new());
+    }
+
+    #[test]
+    fn create_action() {
+        let test_app: ClearHeadApp<Vector<Action>, Vector<Relationship>> = Default::default();
+        let updated_app = test_app.create().unwrap();
+
+        assert_eq!(updated_app.action_list.len(), 1);
     }
 }
