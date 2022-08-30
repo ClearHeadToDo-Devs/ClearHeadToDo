ca5ff5ddefacccd580be8c53a6fb4593d07c8674
Darrion Burgess <dargondab9@gmail.com>
2022-08-29 21:35:02 -0500

    I think this was the first time that copilot just straight up solved the problem for me

--- core/src/functionality.rs
+++ core/src/functionality.rs
@@ -1,22 +1,35 @@
-use std::error::Error;
-
-use action::{action_list_manipulation::ActionListManipulation, ActionManipulation};
-use relationships::{RelationshipListManagement, RelationshipManagement};
+use action::action_list_manipulation::ActionListManipulation;
+use action::item::Action;
+use relationships::{RelationshipListManagement, Relationship};
 
 use im::Vector;
-struct ClearHeadApp<A: ActionListManipulation, R: RelationshipListManagement>  {
+use std::fmt::Debug;
+use std::clone::Clone;
+use std::cmp::PartialEq;
+
+#[derive(Debug, PartialEq)]
+struct ClearHeadApp<A: ActionListManipulation + Clone, R: RelationshipListManagement + Clone>  {
     action_list: Vector<A>,
     relationship_list: Vector<R>,
+}
+
+impl <A: ActionListManipulation + Clone,R: RelationshipListManagement + Clone>Default for ClearHeadApp<A, R> {
+    fn default() -> Self {
+        ClearHeadApp {
+            action_list: Vector::new(),
+            relationship_list: Vector::new(),
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use action::Action;
-    use relationships::Relationship;
+    use action::{Action, ActionManipulation};
+    use relationships::{Relationship, RelationshipManagement};
 
     #[test]
-    fn test_app_creation() {
+    fn app_creation() {
         let test_app: ClearHeadApp<Vector<Action>, Vector<Relationship>> = ClearHeadApp {
             action_list: Vector::new(),
             relationship_list: Vector::new(),
@@ -24,4 +37,12 @@ mod tests {
         assert_eq!(test_app.action_list, Vector::new());
         assert_eq!(test_app.relationship_list, Vector::new());
     }
+
+    #[test]
+    fn default_app_creation() {
+        let test_app: ClearHeadApp<Vector<Action>, Vector<Relationship>> = Default::default();
+        assert_eq!(test_app.action_list, Vector::new());
+        assert_eq!(test_app.relationship_list, Vector::new());
+    }
+
 }
