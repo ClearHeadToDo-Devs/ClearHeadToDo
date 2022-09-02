c3e9db7ecb3010b92c7c310fd3b6588dea9f3860
Darrion Burgess <dargondab9@gmail.com>
2022-09-02 16:13:45 -0500

    I mean, I was able to implement the trait on the app, so this may make sense...

--- core/src/functionality.rs
+++ core/src/functionality.rs
@@ -3,12 +3,16 @@ use relationships::RelationshipListManagement;
 
 use action::Action;
 use action::ActionListManipulation;
+use action::ActionError;
+use action::Priority;
+
 
 use std::fmt::Debug;
 use std::cmp::PartialEq;
 use std::error::Error;
 
 use serde::{Serialize, Deserialize};
+use uuid::Uuid;
 use im::Vector;
 
 
@@ -97,6 +101,102 @@ impl ClearHeadApp {
 
         Ok(cloned_list)
         }
+}
+
+impl ActionListManipulation for ClearHeadApp{
+    fn append_default(&self) -> Self {
+        let mut new_list = self.clone();
+
+        new_list.action_list.push_back(Action::default());
+
+        return new_list;
+    }
+
+    fn remove_action(&self, index: usize) -> Result<ClearHeadApp, Box<dyn Error>> {
+        let mut new_list = self.clone();
+
+        new_list.action_list.remove(index);
+
+        return Ok(new_list);
+    }
+
+    fn rename(
+        &self,
+        index: usize,
+        new_name: String,
+    ) -> Result<ClearHeadApp, Box<dyn Error>> {
+        let mut cloned_app = self.clone();
+
+        let updated_action = self.select_by_index(index)?.rename(&new_name);
+        let updated_list = self.action_list.update(index, updated_action);
+
+        cloned_app.action_list = updated_list;
+        Ok(cloned_app)
+    }
+
+    fn toggle_completion_status(
+        &self,
+        index: usize,
+    ) -> Result<ClearHeadApp, Box<dyn Error>> {
+        let mut cloned_list = self.clone();
+        let updated_action = self.select_by_index(index)?.toggle_completion_status();
+
+        let updated_list = cloned_list.action_list.update(index, updated_action);
+
+        cloned_list.action_list = updated_list;
+
+        Ok(cloned_list)
+    }
+
+    fn change_priority(
+        &self,
+        index: usize,
+        new_priority: String,
+    ) -> Result<ClearHeadApp, Box<dyn Error>> {
+        let mut cloned_list = self.clone();
+
+        let updated_action = self.select_by_index(index)?
+            .change_priority(&new_priority)?;
+        let updated_list = self.action_list.update(index, updated_action);
+
+        cloned_list.action_list = updated_list;
+
+        Ok(cloned_list)
+    }
+
+    fn select_by_id(&self, id: Uuid) -> Result<Action, Box<dyn Error>> {
+        let search_action_result = self.clone().action_list.into_iter()
+            .find(|actions| actions.get_id() == id);
+
+        match search_action_result {
+            Some(action) => return Ok(action.clone().to_owned()),
+            None => {
+                return Err(ActionError::InvalidId(id).into())
+            }
+        }
+    }
+
+    fn select_by_index(&self, index: usize) -> Result<Action, Box<dyn Error>> {
+        match self.action_list.iter().nth(index) {
+            Some(action_ref) => return Ok(action_ref.clone()),
+            None => Err(ActionError::InvalidIndex(index).into()),
+        }
+    }
+
+    fn get_action_id(&self, index: usize) -> Result<Uuid, Box<dyn Error>> {
+        Ok(self.select_by_index(index)?.get_id())
+    }
+
+    fn get_action_name(&self, index: usize) -> Result<String, Box<dyn Error>> {
+        Ok(self.select_by_index(index)?.get_name())
+    }
+
+    fn get_action_priority(&self, index: usize) -> Result<Priority, Box<dyn Error>> {
+        Ok(self.select_by_index(index)?.get_priority())
+    }
+    fn get_action_completion_status(&self, index: usize) -> Result<bool, Box<dyn Error>> {
+        Ok(self.select_by_index(index)?.get_completion_status())
+    }
 }
 
 #[cfg(test)]
