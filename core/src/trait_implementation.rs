787acc53b33d6e71ef4d677b607d4ed199e536e2
Darrion Burgess <dargondab9@gmail.com>
2022-09-05 16:55:34 -0500

    working out the trait implementation quickly since I've done this so many times at this point

--- core/src/trait_implementation.rs
+++ core/src/trait_implementation.rs
@@ -40,21 +40,28 @@ impl ActionListManipulation for ClearHeadApp {
     fn rename(&self, index: usize, new_name: String) -> Result<Self, Box<dyn Error>>
         where
             Self: Sized {
-        unimplemented!();
+        let mut updated_list = self.clone();
+
+        updated_list.action_list = updated_list.action_list.rename(index, new_name)?;
+
+        Ok(updated_list)
     }
 
     fn toggle_completion_status(&self, index: usize) -> Result<Self, Box<dyn Error>>
         where
             Self: Sized {
-        unimplemented!();
+        let mut updated_list = self.clone();
+
+        updated_list.action_list = updated_list.action_list.toggle_completion_status(index)?;
+        Ok(updated_list)
     }
 
     fn select_by_id(&self, id: Uuid) -> Result<Action, Box<dyn Error>> {
-        unimplemented!();
+        self.action_list.select_by_id(id)
     }
 
     fn select_by_index(&self, index: usize) -> Result<Action, Box<dyn Error>> {
-        unimplemented!();
+        self.action_list.select_by_index(index)
     }
 
     fn get_action_name(&self, index: usize) -> Result<String, Box<dyn Error>> {
@@ -66,24 +73,28 @@ impl ActionListManipulation for ClearHeadApp {
     }
 
     fn get_action_completion_status(&self, index: usize) -> Result<bool, Box<dyn Error>> {
-        unimplemented!();
+        self.action_list.get_action_completion_status(index)
     }
 
     fn get_action_id(&self, index: usize) -> Result<Uuid, Box<dyn Error>> {
-        unimplemented!();
+        self.action_list.get_action_id(index)
     }
 
     fn remove_action(&self, index: usize) -> Result<Self, Box<dyn Error>>
         where
             Self: Sized {
-        unimplemented!();
+        let mut updated_app = self.clone();
+
+        updated_app.action_list = self.action_list.remove_action(index)?;
+
+        Ok(updated_app)
     }
 
 }
 
 #[cfg(test)]
 mod tests{
-    use crate::{ClearHeadApp, functionality::tests::{create_app_with_single_action, failed_action_index_error}};
+    use crate::{ClearHeadApp, functionality::tests::{create_app_with_single_action, failed_action_index_error, get_first_action}};
 
     use super::*;
 
@@ -131,5 +142,123 @@ mod tests{
         let index_error = empty_app.get_action_priority(0).unwrap_err();
 
         assert_eq!(index_error.to_string(), failed_action_index_error(0));
+    }
+
+    #[test]
+    fn get_action_completion_status(){
+        let test_app = create_app_with_single_action();
+
+        let action_completion_status = test_app.get_action_completion_status(0);
+
+        assert_eq!(action_completion_status.unwrap(), false);
+    }
+
+    #[test]
+    fn failed_get_action_completion_status(){
+        let empty_app = ClearHeadApp::default();
+
+        let index_error = empty_app.get_action_completion_status(0).unwrap_err();
+
+        assert_eq!(index_error.to_string(), failed_action_index_error(0));
+    }
+
+    #[test]
+    fn get_action_id(){
+        let test_app = create_app_with_single_action();
+
+        let action_id = test_app.get_action_id(0).unwrap();
+
+        assert_eq!(action_id, get_first_action(&test_app).get_id());
+    }
+
+    #[test]
+    fn failed_get_action_id(){
+        let empty_app = ClearHeadApp::default();
+
+        let index_error = empty_app.get_action_id(0).unwrap_err();
+
+        assert_eq!(index_error.to_string(), failed_action_index_error(0));
+    }
+
+    #[test]
+    fn get_action_by_index(){
+        let test_app = create_app_with_single_action();
+
+        let action = test_app.select_by_index(0).unwrap();
+
+        assert_eq!(action, get_first_action(&test_app));
+    }
+
+    #[test]
+    fn failed_get_action_by_index(){
+        let empty_app = ClearHeadApp::default();
+
+        let index_error = empty_app.select_by_index(0).unwrap_err();
+
+        assert_eq!(index_error.to_string(), failed_action_index_error(0));
+    }
+
+    #[test]
+    fn get_action_by_id(){
+        let test_app = create_app_with_single_action();
+
+        let action = test_app.select_by_id(test_app.action_list[0].get_id()).unwrap();
+
+        assert_eq!(action, get_first_action(&test_app));
+    }
+
+    #[test]
+    fn failed_get_action_by_id(){
+        let empty_app = ClearHeadApp::default();
+
+        let index_error = empty_app.select_by_id(Uuid::nil()).unwrap_err();
+
+        let expected_error = format!("No Action with Id {}", Uuid::nil());
+        assert_eq!(index_error.to_string(), expected_error);
+    }
+
+    #[test]
+    fn remove_action(){
+        let test_app = create_app_with_single_action();
+
+        let updated_app = test_app.remove_action(0).unwrap();
+
+        assert_eq!(updated_app.action_list.len(), 0);
+    }
+
+    #[test]
+    fn failed_remove_action(){
+        let empty_app = ClearHeadApp::default();
+
+        let index_error = empty_app.remove_action(0).unwrap_err();
+
+        assert_eq!(index_error.to_string(), failed_action_index_error(0));
+    }
+
+    #[test]
+    fn change_action_name(){
+        let test_app = create_app_with_single_action();
+
+        let updated_app = test_app.rename(0, "New Name".to_string()).unwrap();
+
+        assert_eq!(get_first_action(&updated_app).get_name(), "New Name");
+    }
+
+    #[test]
+    fn failed_change_action_name(){
+        let empty_app = ClearHeadApp::default();
+
+        let index_error = empty_app.rename(0, "New Name".to_string()).unwrap_err();
+
+        assert_eq!(index_error.to_string(), failed_action_index_error(0));
+    }
+
+    #[test]
+    fn toggle_action_completion_status(){
+        let test_app = create_app_with_single_action();
+
+        let updated_app = test_app.toggle_completion_status(0).unwrap();
+
+        assert_eq!(get_first_action(&updated_app).get_completion_status(), true);
     }
 }
