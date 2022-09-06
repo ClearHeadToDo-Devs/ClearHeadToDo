a933f881b4998b7362af94718e5366825379000f
Darrion Burgess <dargondab9@gmail.com>
2022-09-05 22:16:23 -0500

    removed the duplicate functions, but now I need to fix the errors, this isn't how I like to leave things but I think I have built enough stability to handle this change

--- core/src/functionality.rs
+++ core/src/functionality.rs
@@ -23,49 +23,6 @@ pub struct ClearHeadApp  {
 }
 
 impl ClearHeadApp {
-    pub fn create_action(&self) -> ClearHeadApp  {
-        let mut cloned_list = self.clone();
-
-        let new_action_list = self.action_list.append_default();
-        cloned_list.action_list = new_action_list;
-
-        cloned_list
-        }
-    pub fn rename_action(&self, index: usize, new_name: String) -> Result<ClearHeadApp, Box<dyn Error>> {
-        let mut cloned_list = self.clone();
-
-        let new_action_list = cloned_list.action_list.rename(index, new_name)?;
-        cloned_list.action_list = new_action_list;
-
-        Ok(cloned_list)
-        }
-
-    pub fn toggle_action_completion_status(&self, index: usize) -> Result<ClearHeadApp, Box<dyn Error>> {
-        let mut cloned_list = self.clone();
-
-        let new_action_list = cloned_list.action_list.toggle_completion_status(index)?;
-        cloned_list.action_list = new_action_list;
-
-        Ok(cloned_list)
-        }
-
-    pub fn change_action_priority(&self, index: usize, new_priority: String) -> Result<ClearHeadApp, Box<dyn Error>> {
-        let mut cloned_list = self.clone();
-
-        let new_action_list = cloned_list.action_list.change_priority(index, new_priority)?;
-        cloned_list.action_list = new_action_list;
-
-        Ok(cloned_list)
-        }
-
-    pub fn remove_action(&self, index: usize) -> Result<ClearHeadApp, Box<dyn Error>> {
-        let mut cloned_list = self.clone();
-
-        let new_action_list = cloned_list.action_list.remove_action(index)?;
-        cloned_list.action_list = new_action_list;
-
-        Ok(cloned_list)
-        }
 
     pub fn get_list(&self) -> String {
         Table::new(&self.action_list).to_string()
@@ -91,59 +48,20 @@ impl ClearHeadApp {
         Ok(extended_list)
     }
 
-    pub fn get_relationship_id(&self, index: usize) -> Result<Uuid, Box<dyn Error>> {
-        Ok(self.relationship_list.get_id(index)?)
+    fn create_action_relationship(&self, variant_str: &str, participant_1_index: usize, participant_2_index: usize) -> Result<ClearHeadApp, Box<dyn Error>> {
+        let mut cloned_app = self.clone();
+
+        let participant_1_id = self.get_action_id(participant_1_index)?;
+        let participant_2_id = self.get_action_id(participant_2_index)?;
+
+        let updated_list = self.add_new(variant_str, participant_1_id, participant_2_id)?;
+
+        cloned_app.relationship_list = updated_list;
+
+        Ok(cloned_app)
     }
-
-    pub fn get_relationship_variant(&self, index: usize) -> Result<RelationshipVariant, Box<dyn Error>> {
-        Ok(self.relationship_list.get_variant(index)?)
-    }
-
-    pub fn get_relationship_participant_1(&self, index: usize) -> Result<Uuid, Box<dyn Error>> {
-        Ok(self.relationship_list.get_participant_1(index)?)
-    }
-
-    pub fn get_relationship_participant_2(&self, index: usize) -> Result<Uuid, Box<dyn Error>> {
-        Ok(self.relationship_list.get_participant_2(index)?)
-    }
-    
-
-    pub fn create_relationship(&self, variant: &str, participant_1: usize, participant_2: usize) -> Result<ClearHeadApp, Box<dyn Error>> {
-        let mut cloned_list = self.clone();
-        let participant_1_id = self.action_list.select_by_index(participant_1)?.get_id();
-        let participant_2_id = self.action_list.select_by_index(participant_2)?.get_id();
-
-        let updated_relationship_list: Vector<Relationship> = 
-        self.relationship_list.add_new(
-            variant, participant_1_id, participant_2_id)?;
-
-        cloned_list.relationship_list = updated_relationship_list;
-
-        Ok(cloned_list)
-        }
-
-    pub fn change_relationship_variant(&self, index: usize, new_variant: &str) -> Result<ClearHeadApp, Box<dyn Error>> {
-        let mut cloned_list = self.clone();
-
-        let updated_relationship_list: Vector<Relationship> = 
-        self.relationship_list.change_variant(index, new_variant)?;
-
-        cloned_list.relationship_list = updated_relationship_list;
-
-        Ok(cloned_list)
-        }
-
-    pub fn change_relationship_participant_1(&self, index: usize, new_participant_1_index: usize) -> Result<ClearHeadApp, Box<dyn Error>> {
-        let mut cloned_list = self.clone();
-
-        let updated_relationship_list: Vector<Relationship> = 
-        self.relationship_list.update_participant_1(index, self.action_list.select_by_index(new_participant_1_index)?.get_id())?;
-
-        cloned_list.relationship_list = updated_relationship_list;
-
-        Ok(cloned_list)
-        }
 }
+
 
 
 #[cfg(test)]
@@ -156,13 +74,13 @@ pub mod tests {
     use relationships::item::RelationshipVariant;
 
     pub fn create_app_with_single_action() -> ClearHeadApp {
-        let app = ClearHeadApp::default().create_action();
+        let app = ClearHeadApp::default().append_default();
 
         app
     }
 
     pub fn create_app_with_two_actions() -> ClearHeadApp {
-        let app = ClearHeadApp::default().create_action().create_action();
+        let app = ClearHeadApp::default().append_default().append_default();
 
         app
     }
@@ -181,9 +99,9 @@ pub mod tests {
 
     pub fn create_minimal_related_app(variant_str: &str) -> ClearHeadApp {
         let app = ClearHeadApp::default()
-            .create_action()
-            .create_action()
-            .create_relationship(variant_str, 0 , 1).unwrap();
+            .append_default()
+            .append_default()
+            .create_action_relationship(variant_str, 0, 1).unwrap();
 
         app
     }
@@ -195,78 +113,6 @@ pub mod tests {
 
         assert_eq!(test_app.action_list, Vector::new());
         assert_eq!(test_app.relationship_list, Vector::new());
-    }
-
-    #[test]
-    fn create_action() {
-        let test_app: ClearHeadApp = Default::default();
-
-        let updated_app = test_app.create_action();
-
-        assert_eq!(updated_app.action_list.len(), 1);
-    }
-
-    #[test]
-    fn successful_rename_action(){
-        let test_app = create_app_with_single_action();
-
-        let updated_app = test_app.rename_action(0, "new_name".to_string()).unwrap();
-
-        assert_eq!(get_first_action(&updated_app).get_name(), "new_name");
-    }
-
-    #[test]
-    fn failed_rename_action(){
-        let test_app = ClearHeadApp::default();
-
-        let failed_update = test_app.rename_action(0, "new_name".to_string()).unwrap_err();
-
-        assert_eq!(failed_update.to_string(), failed_action_index_error(0));
-    }
-
-    #[test]
-    fn toggle_action_completion_status(){
-        let test_app = create_app_with_single_action();
-
-        let updated_app = test_app.toggle_action_completion_status(0).unwrap();
-
-        assert_eq!(get_first_action(&updated_app).get_completion_status(), true);
-    }
-
-    #[test]
-    fn failed_toggle_action_completion_status(){
-        let test_app = ClearHeadApp::default();
-
-        let failed_update = test_app.toggle_action_completion_status(0).unwrap_err();
-
-        assert_eq!(failed_update.to_string(), failed_action_index_error(0));
-    }
-
-    #[test]
-    fn successful_remove_action(){
-        let test_app = create_app_with_single_action();
-
-        let updated_app = test_app.remove_action(0).unwrap();
-
-        assert_eq!(updated_app.action_list.len(), 0);
-    }
-
-    #[test]
-    fn failed_remove_action(){
-        let test_app = ClearHeadApp::default();
-
-        let index_error = test_app.remove_action(0).unwrap_err();
-
-        assert_eq!(index_error.to_string(), failed_action_index_error(0));
-    }
-
-    #[test]
-    fn change_action_priority(){
-        let test_app = create_app_with_single_action();
-
-        let updated_app = test_app.change_action_priority(0, 1.to_string()).unwrap();
-
-        assert_eq!(get_first_action(&updated_app).get_priority(), Priority::Critical);
     }
 
     #[test]
@@ -306,158 +152,5 @@ pub mod tests {
 
         assert_eq!(all_actions, expected_string);
 
-    }
-
-    #[test]
-    fn get_relationship_id(){
-        let test_app = create_minimal_related_app("parental");
-
-        let relationship_id = test_app.get_relationship_id(0).unwrap();
-
-        assert_eq!(relationship_id, test_app.relationship_list[0].get_id());
-    }
-
-    #[test]
-    fn failed_get_relationship_id(){
-        let test_app = ClearHeadApp::default();
-
-        let failed_get = test_app.get_relationship_id(0).unwrap_err();
-
-        assert_eq!(failed_get.to_string(), failed_relationship_index_error());
-    }
-
-    #[test]
-    fn get_relationship_variant(){
-        let test_app = create_minimal_related_app("parental");
-
-        let relationship_variant = test_app.get_relationship_variant(0).unwrap();
-
-        assert_eq!(relationship_variant, RelationshipVariant::create_parental());
-    }
-
-    #[test]
-    fn failed_get_relationship_variant(){
-        let test_app = ClearHeadApp::default();
-
-        let failed_get = test_app.get_relationship_variant(0).unwrap_err();
-
-        assert_eq!(failed_get.to_string(), failed_relationship_index_error());
-    }
-
-    #[test]
-    fn get_relationship_participant_1(){
-        let test_app = create_minimal_related_app("parental");
-
-        let relationship_participant_1 = test_app.get_relationship_participant_1(0).unwrap();
-
-        assert_eq!(relationship_participant_1, test_app.relationship_list.get_participant_1(0).unwrap());
-    }
-
-    #[test]
-    fn failed_get_relationship_participant_1(){
-        let test_app = ClearHeadApp::default();
-
-        let failed_get = test_app.get_relationship_participant_1(0).unwrap_err();
-
-        assert_eq!(failed_get.to_string(), failed_relationship_index_error());
-    }
-
-    #[test]
-    fn get_relationship_participant_2(){
-        let test_app = create_minimal_related_app("parental");
-
-        let relationship_participant_2 = test_app.get_relationship_participant_2(0).unwrap();
-
-        assert_eq!(relationship_participant_2, test_app.relationship_list.get_participant_2(0).unwrap());
-    }
-
-    #[test]
-    fn failed_get_relationship_participant_2(){
-        let test_app = ClearHeadApp::default();
-
-        let failed_get = test_app.get_relationship_participant_2(0).unwrap_err();
-
-        assert_eq!(failed_get.to_string(), failed_relationship_index_error());
-    }
-
-    #[test]
-    fn create_relationship(){
-        let test_app: ClearHeadApp = create_app_with_two_actions();
-
-        let updated_app = test_app.create_relationship("related", 0,1).unwrap();
-
-        assert_eq!(updated_app.relationship_list.len(), 1);
-    }
-
-    #[test]
-    fn create_sequential_relationship(){
-        let test_app = create_app_with_two_actions();
-
-        let updated_app = test_app.create_relationship("sequential", 0,1).unwrap();
-
-        assert_eq!(updated_app.relationship_list.len(), 1);
-    }
-
-    #[test]
-    fn create_parental_relationship(){
-        let test_app = create_app_with_two_actions();
-
-        let updated_app = test_app.create_relationship("parental", 0,1).unwrap();
-
-        assert_eq!(updated_app.relationship_list.len(), 1);
-    }
-
-    #[test]
-    fn failed_non_existant_action_relationship(){
-        let test_app: ClearHeadApp = Default::default();
-
-        let invalid_index_error = test_app.create_relationship("related", 0, 1).unwrap_err();
-
-        assert_eq!(invalid_index_error.to_string(), failed_action_index_error(0));
-    }
-
-    #[test]
-    fn failed_invalid_relationship_type(){
-        let test_app = create_app_with_two_actions();
-
-        let invalid_relationship_error = test_app.create_relationship("invalid", 0, 1).unwrap_err();
-
-        assert_eq!(invalid_relationship_error.to_string(), "invalid relationship variant");
-    }
-
-    #[test]
-    fn change_relationship_to_parental(){
-        let test_app = create_minimal_related_app("related");
-
-        let updated_app = test_app.change_relationship_variant(0, "parental").unwrap();
-
-        assert_eq!(updated_app.relationship_list[0].get_variant(), RelationshipVariant::create_parental());
-    }
-
-    #[test]
-    fn change_relationship_variant_to_sequential(){
-        let test_app = create_minimal_related_app("related");
-
-        let updated_app = test_app.change_relationship_variant(0, "sequential").unwrap();
-
-        assert_eq!(updated_app.get_relationship_variant(0).unwrap(), RelationshipVariant::create_sequential());
-    }
-
-    #[test]
-    fn change_relationship_variant_to_related(){
-        let test_app = create_minimal_related_app("parental");
-
-        let updated_app = test_app.change_relationship_variant(0, "related").unwrap();
-
-        assert_eq!(updated_app.relationship_list[0].get_variant(), RelationshipVariant::create_related());
-    }
-
-    #[test]
-    fn change_participant_1(){
-        let test_app = create_minimal_related_app("parental").create_action();
-
-        let updated_app = test_app.change_relationship_participant_1(0, 2).unwrap();
-
-        assert_eq!(updated_app.get_relationship_participant_1(0).unwrap(), test_app.action_list.get_action_id(2).unwrap());
     }
 }
