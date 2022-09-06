5503216d460292724dc5926761db311b29985cf8
Darrion Burgess <dargondab9@gmail.com>
2022-09-06 10:03:33 -0500

    working out the testing of the command_runner

--- cli/src/command_runner.rs
+++ cli/src/command_runner.rs
@@ -134,7 +134,9 @@ impl Command {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use clear_head_todo_core::Priority;
+    use clear_head_todo_core::{Priority, RelationshipListManagement};
+    
+
 
     fn create_empty_and_singe_action_list() -> (ClearHeadApp, ClearHeadApp) {
         let empty_list = ClearHeadApp::default();
@@ -147,6 +149,12 @@ mod tests {
         let single_action_list = ClearHeadApp::default().append_default();
 
         single_action_list
+    }
+
+    fn create_double_action_app() -> ClearHeadApp {
+        let double_action_app = ClearHeadApp::default().append_default().append_default();
+
+        double_action_app
     }
 
     #[test]
@@ -254,14 +262,13 @@ mod tests {
 
     #[test]
     fn cli_rename_successful_run_test() {
-        let empty_list: ClearHeadApp = Default::default();
-        let single_list = empty_list.append_default();
+        let single_action_app = create_single_action_app();
 
         let result = Command::Rename {
             index: 0,
             new_name: "Test Rename".to_string(),
         }
-        .run_subcommand(&single_list).unwrap();
+        .run_subcommand(&single_action_app).unwrap();
 
         assert_eq!(
             result.action_list[0].get_name(),
@@ -271,8 +278,7 @@ mod tests {
 
     #[test]
     fn generate_rename_message() {
-        let empty_app = ClearHeadApp::default();
-        let single_action_app = empty_app.append_default();
+        let single_action_app = create_single_action_app();
         let updated_list = single_action_app
             .rename(0, "New Name".to_string())
             .unwrap();
@@ -295,26 +301,25 @@ mod tests {
             new_name: "Test Rename".to_string(),
         }
         .run_subcommand(&empty_list);
+
         assert_eq!(error.unwrap_err().to_string(), "No Action at Index 0");
     }
 
     #[test]
     fn cli_change_priority_successful_run_test() {
-        let empty_list: ClearHeadApp = Default::default();
-        let single_list = empty_list.append_default();
+        let single_action_app = create_single_action_app();
 
         let result = Command::Reprioritize {
             index: 0,
             new_priority: "High".to_string(),
         }
-        .run_subcommand(&single_list).unwrap();
-        assert_eq!(result.action_list[0].get_priority(), Priority::High);
+        .run_subcommand(&single_action_app).unwrap();
+        assert_eq!(result.get_action_priority(0).unwrap(), Priority::High);
     }
 
     #[test]
     fn generate_reprioritize_message() {
-        let empty_app = ClearHeadApp::default();
-        let single_action_app = empty_app.append_default();
+        let single_action_app = create_single_action_app();
         let updated_list = single_action_app
             .change_priority(0, "low".to_string())
             .unwrap();
@@ -327,7 +332,8 @@ mod tests {
 
         assert_eq!(
             message,
-            "Default Action was changed from a priority of: Optional\n to a priority of: low"
+"Default Action was changed from a priority of: Optional
+ to a priority of: low"
         );
     }
 
@@ -340,66 +346,65 @@ mod tests {
             new_priority: "High".to_string(),
         }
         .run_subcommand(&empty_list);
+
         assert_eq!(error.unwrap_err().to_string(), "No Action at Index 1");
     }
 
     #[test]
     fn cli_create_related_relationship_successful_run() {
-        let empty_list: ClearHeadApp = Default::default();
-        let updated_list = empty_list.append_default().append_default();
+        let double_action_app = create_double_action_app();
 
         let result = Command::CreateRelationship {
             variant: "related".to_string(),
             participant_1: 0,
             participant_2: 1,
         }
-        .run_subcommand(&updated_list).unwrap();
+        .run_subcommand(&double_action_app).unwrap();
+
         assert_eq!(result.relationship_list.len(), 1);
-        assert_eq!(result.relationship_list[0].get_variant().to_string(),
-            "Related: Undirected" );
-        assert_eq!(result.relationship_list[0].get_participant_1(), result.action_list[0].get_id());
-        assert_eq!(result.relationship_list[0].get_participant_2(), result.action_list[1].get_id());
+        assert_eq!(result.get_variant(0).unwrap().to_string(), "Related: Undirected");
+        assert_eq!(result.get_participant_1(0).unwrap(), result.get_action_id(0).unwrap());
+        assert_eq!(result.get_participant_2(0).unwrap(), result.get_action_id(1).unwrap());
     }
 
     #[test]
     fn cli_create_sequential_relationship_successful_run() {
-        let empty_list: ClearHeadApp = Default::default();
-        let updated_list = empty_list.append_default().append_default();
+        let double_action_app = create_double_action_app();
 
         let result = Command::CreateRelationship {
             variant: "sequential".to_string(),
             participant_1: 0,
             participant_2: 1,
         }
-        .run_subcommand(&updated_list).unwrap();
-        assert_eq!(result.relationship_list[0].get_variant().to_string(), "Sequential: Directed");
+        .run_subcommand(&double_action_app).unwrap();
+
+        assert_eq!(result.get_variant(0).unwrap().to_string(), "Sequential: Directed");
     }
 
     #[test]
     fn cli_create_parental_relationship_successful_run() {
-        let empty_list: ClearHeadApp = Default::default();
-        let updated_list = empty_list.append_default().append_default();
+        let double_action_app = create_double_action_app();
 
         let result = Command::CreateRelationship {
             variant: "parental".to_string(),
             participant_1: 0,
             participant_2: 1,
         }
-        .run_subcommand(&updated_list).unwrap();
-        assert_eq!(result.relationship_list[0].get_variant().to_string(), "Parental: Directed");
+        .run_subcommand(&double_action_app).unwrap();
+
+        assert_eq!(result.get_variant(0).unwrap().to_string(), "Parental: Directed");
     }
 
     #[test]
     fn cli_create_relationship_successful_message() {
-        let empty_list: ClearHeadApp = ClearHeadApp::default().append_default().append_default();
-        let single_relationship_app = empty_list.create_action_relationship("related", 0, 1).unwrap();
-        let updated_list = single_relationship_app.append_default().append_default();
+        let double_action_app = create_double_action_app();
+        let single_relationship_app = double_action_app.create_action_relationship("related", 0, 1).unwrap();
 
         let result = Command::CreateRelationship {
             variant: "related".to_string(),
             participant_1: 0,
             participant_2: 1,
-        }.create_end_user_message(&single_relationship_app, &updated_list);
+        }.create_end_user_message(&double_action_app, &single_relationship_app);
 
         assert_eq!(result, "Created related Relationship from Action 0 to Action 1");
     }
