ad3587c803dcd6e996941bbdc9a360ffb6d73ffc
Darrion Burgess <dargondab9@gmail.com>
2022-12-11 16:29:05 -0600

    just doing some formatting

--- core/src/functionality.rs
+++ core/src/functionality.rs
@@ -2,26 +2,25 @@ use crate::relationship::Relationship;
 use crate::relationship::RelationshipListManagement;
 
 use crate::action::Action;
-use crate::ActionManipulation;
 use crate::action_implementation::ActionListManipulation;
-use tabled::Modify;
+use crate::ActionManipulation;
+
 use tabled::object::Rows;
 use tabled::Alignment;
 use tabled::Footer;
 use tabled::Header;
+use tabled::Modify;
 
-
-use std::fmt::Debug;
 use std::cmp::PartialEq;
 use std::error::Error;
+use std::fmt::Debug;
 use tabled::Table;
 
-use serde::{Serialize, Deserialize};
 use im::Vector;
-
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, PartialEq, Default, Clone, Serialize, Deserialize)]
-pub struct ClearHeadApp  {
+pub struct ClearHeadApp {
     pub action_list: Vector<Action>,
     pub relationship_list: Vector<Relationship>,
 }
@@ -29,7 +28,8 @@ pub struct ClearHeadApp  {
 impl ClearHeadApp {
     pub fn get_list(&self) -> Table {
         Table::builder(&self.action_list)
-            .index().build()
+            .index()
+            .build()
             .with(Header("Action List"))
             .with(Modify::new(Rows::first()).with(Alignment::center()))
             .with(Footer(format!("{} Item(s)", self.action_list.len())))
@@ -42,26 +42,35 @@ impl ClearHeadApp {
 
         extended_list.push_str("Order,Name,Priority,Completed,Id\n");
         for action in &self.action_list {
-
-            extended_list.push_str(&format!("{},{}\n",index, action.to_string()));
+            extended_list.push_str(&format!("{},{}\n", index, action.to_string()));
             index += 1;
 
             if self.id_is_present_in_participant_1_list(action.get_id()) {
                 for relationship in &self.get_participant_1_list_for_id(action.get_id())? {
-                        extended_list.push_str(&format!(
-                            "  - {},{}\n",relationship.get_variant(), 
-                            &self.action_list.select_action_by_id(relationship.get_participant_2())?));
-                    }
+                    extended_list.push_str(&format!(
+                        "  - {},{}\n",
+                        relationship.get_variant(),
+                        &self
+                            .action_list
+                            .select_action_by_id(relationship.get_participant_2())?
+                    ));
                 }
             }
+        }
         Ok(extended_list)
     }
 
-    pub fn create_action_relationship(&self, variant_str: &str, participant_1_index: usize, participant_2_index: usize) -> Result<ClearHeadApp, Box<dyn Error>> {
+    pub fn create_action_relationship(
+        &self,
+        variant_str: &str,
+        participant_1_index: usize,
+        participant_2_index: usize,
+    ) -> Result<ClearHeadApp, Box<dyn Error>> {
         let participant_1_id = self.get_action_id(participant_1_index)?;
         let participant_2_id = self.get_action_id(participant_2_index)?;
 
-        let updated_app = self.append_new_relationship(variant_str, participant_1_id, participant_2_id)?;
+        let updated_app =
+            self.append_new_relationship(variant_str, participant_1_id, participant_2_id)?;
 
         Ok(updated_app)
     }
@@ -71,8 +80,7 @@ impl ClearHeadApp {
 pub mod tests {
     use super::*;
     use im::Vector;
-    use indoc::{indoc, formatdoc};
-
+    use indoc::{formatdoc, indoc};
 
     pub fn create_app_with_single_action() -> ClearHeadApp {
         let app = ClearHeadApp::default().append_default_action();
@@ -81,7 +89,9 @@ pub mod tests {
     }
 
     pub fn create_app_with_two_actions() -> ClearHeadApp {
-        let app = ClearHeadApp::default().append_default_action().append_default_action();
+        let app = ClearHeadApp::default()
+            .append_default_action()
+            .append_default_action();
 
         app
     }
@@ -91,7 +101,7 @@ pub mod tests {
     }
 
     pub fn failed_action_index_error(index: usize) -> String {
-        format!("No Action at Index {}",index.to_string())
+        format!("No Action at Index {}", index.to_string())
     }
 
     pub fn failed_relationship_index_error() -> String {
@@ -102,11 +112,11 @@ pub mod tests {
         let app = ClearHeadApp::default()
             .append_default_action()
             .append_default_action()
-            .create_action_relationship(variant_str, 0, 1).unwrap();
+            .create_action_relationship(variant_str, 0, 1)
+            .unwrap();
 
         app
     }
-
 
     #[test]
     fn default_app_creation() {
@@ -117,12 +127,13 @@ pub mod tests {
     }
 
     #[test]
-    fn list_single_action_table(){
+    fn list_single_action_table() {
         let test_app = create_app_with_single_action();
 
         let action_list_string = test_app.get_list();
 
-        let expected_string = indoc!("
+        let expected_string = indoc!(
+            "
             +---+----------------+----------+-----------+
             |                Action List                |
             +---+----------------+----------+-----------+
@@ -131,19 +142,20 @@ pub mod tests {
             | 0 | Default Action | Optional | false     |
             +---+----------------+----------+-----------+
             |                 1 Item(s)                 |
-            +---+----------------+----------+-----------+");
+            +---+----------------+----------+-----------+"
+        );
 
         assert_eq!(action_list_string.to_string(), expected_string);
-
     }
 
     #[test]
-    fn list_double_action_table(){
+    fn list_double_action_table() {
         let test_app = create_app_with_two_actions();
 
         let action_list_string = test_app.get_list();
 
-        let expected_string = indoc!("
+        let expected_string = indoc!(
+            "
             +---+----------------+----------+-----------+
             |                Action List                |
             +---+----------------+----------+-----------+
@@ -154,27 +166,28 @@ pub mod tests {
             | 1 | Default Action | Optional | false     |
             +---+----------------+----------+-----------+
             |                 2 Item(s)                 |
-            +---+----------------+----------+-----------+");
+            +---+----------------+----------+-----------+"
+        );
 
         assert_eq!(action_list_string.to_string(), expected_string);
-
     }
 
     #[test]
-    fn list_all_actions_with_relationships(){
+    fn list_all_actions_with_relationships() {
         let test_app = create_minimal_related_app("parental");
 
         let all_actions = test_app.get_extended_list().unwrap();
 
-
-        let expected_string = formatdoc!("
+        let expected_string = formatdoc!(
+            "
             Order,Name,Priority,Completed,Id
             0,Default Action,Optional,false,{}
               - Parental: Directed,Default Action,Optional,false,{}
-            1,Default Action,Optional,false,{}\n", 
-                test_app.action_list[0].get_id(),
-                test_app.action_list[1].get_id(),
-                test_app.action_list[1].get_id());
+            1,Default Action,Optional,false,{}\n",
+            test_app.action_list[0].get_id(),
+            test_app.action_list[1].get_id(),
+            test_app.action_list[1].get_id()
+        );
 
         assert_eq!(all_actions, expected_string);
     }
