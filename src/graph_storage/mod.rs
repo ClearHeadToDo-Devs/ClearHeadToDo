d079ceebb0b985a273d8b3af758a3803e4296a8a
Darrion Burgess <dargondab9@gmail.com>
2023-02-05 11:46:03 -0800

    got it working now

--- src/graph_storage/mod.rs
+++ src/graph_storage/mod.rs
@@ -1,20 +1,28 @@
 use crate::priority::Priority;
 use core::str::FromStr;
-use indradb::VertexProperties;
-use indradb::{Identifier, NamedProperty, Vertex, VertexProperty};
+use indradb::{Identifier, NamedProperty, Vertex, VertexProperties, VertexProperty};
 use serde_json::{Number, Value};
-use uuid::Uuid;
 
-pub fn create_name_property(value: Value) -> NamedProperty {
-    NamedProperty::new(create_identifier("Name"), value)
+pub fn create_full_action_vertex(
+    vertex: Vertex,
+    properties: Vec<NamedProperty>,
+) -> VertexProperties {
+    VertexProperties::new(vertex, properties)
 }
 
-pub fn create_boolean_property(vertex_id: Uuid, value: bool) -> VertexProperty {
-    VertexProperty::new(vertex_id, Value::Bool(value))
+pub fn create_name_property(value: &str) -> NamedProperty {
+    let name_identifier = create_identifier("Name");
+    let string_value = create_string_json_value(value);
+
+    NamedProperty::new(name_identifier, string_value)
 }
 
-pub fn create_numeric_property(vertex_id: Uuid, value: Number) -> VertexProperty {
-    VertexProperty::new(vertex_id, Value::Number(value))
+pub fn create_completed_property(value: bool) -> NamedProperty {
+    NamedProperty::new(create_identifier("completed"), Value::Bool(value))
+}
+
+pub fn create_numeric_property(value: Number) -> NamedProperty {
+    NamedProperty::new(create_identifier("Priority"), Value::Number(value))
 }
 
 pub fn create_action_vertex() -> Vertex {
@@ -49,21 +57,40 @@ mod test {
     use super::*;
 
     #[test]
-    fn create_example_name_property() {
-        let name_value = create_string_json_value("test name");
+    fn create_full_action_vertex_example() {
+        let test_vertex = create_action_vertex();
 
-        let name_property = create_name_property(name_value);
+        let name_property = create_name_property("test name");
+        let completed_property = create_completed_property(false);
+        let priority_property = create_numeric_property(Priority::Critical.into());
+
+        let test_propertied_vertex = create_full_action_vertex(
+            test_vertex,
+            vec![name_property, completed_property, priority_property],
+        );
+
+        assert!(test_propertied_vertex.vertex.t == create_identifier("Action"));
+        assert!(test_propertied_vertex.props[0].name.as_str() == "Name");
+        assert!(test_propertied_vertex.props[0].value.as_str().unwrap() == "test name");
+        assert!(test_propertied_vertex.props[1].name.as_str() == "completed");
+        assert!(test_propertied_vertex.props[1].value.as_bool().unwrap() == false);
+        assert!(test_propertied_vertex.props[2].name.as_str() == "Priority");
+        assert!(test_propertied_vertex.props[2].value.as_u64().unwrap() == 1)
+    }
+
+    #[test]
+    fn create_example_name_property() {
+        let name_property = create_name_property("test name");
 
         assert!(name_property.name.as_str() == "Name");
         assert!(name_property.value.as_str().unwrap() == "test name")
     }
 
     #[test]
-    fn create_completed_property() {
-        let test_vertex = create_action_vertex();
+    fn create_example_completed_property() {
+        let completed_property = create_completed_property(false);
 
-        let completed_property = create_boolean_property(test_vertex.id, false);
-
+        assert!(completed_property.name.as_str() == "completed");
         assert!(completed_property.value.as_bool().unwrap() == false)
     }
 
@@ -71,7 +98,7 @@ mod test {
     fn create_priority_property() {
         let test_vertex = create_action_vertex();
 
-        let priority_property = create_numeric_property(test_vertex.id, Priority::Critical.into());
+        let priority_property = create_numeric_property(Priority::Critical.into());
 
         assert!(priority_property.value.as_u64().unwrap() == 1)
     }
