b3f29b5e7e030e6582149b624b34582a74531fd5
Darrion Burgess <dargondab9@gmail.com>
2023-02-05 07:57:13 -0800

    was having too much trouble trying to do the action all at once so I need to break it into smaller pieces before I create it

--- src/graph_storage/mod.rs
+++ src/graph_storage/mod.rs
@@ -1,25 +1,6 @@
 use crate::{action::Action, action_interface::ActionViewing, priority::Priority};
-use indradb::{Identifier, Vertex, VertexProperties, VertexProperty, Json};
-use serde_json::{Value, Number};
-
-impl From<Action> for VertexProperties {
-    fn from(value: Action) -> VertexProperties {
-        let vertex = Vertex::new(Identifier::new("Action".to_string()).unwrap());
-        let vertex_id = vertex.id.clone();
-
-        let properties = vec![
-            VertexProperty::new(vertex_id, Value::String(value.get_name().to_string())),
-            VertexProperty::new(vertex_id, Value::Bool(value.get_completion_status())),
-            VertexProperty::new(vertex_id, Value::Number(value.get_priority().into()))
-        ];
-    }
-}
-
-impl From<&Priority> for Value {
-    fn from(val: &Priority) -> Self {
-        Value::Number(1)
-    }
-}
+use indradb::{Identifier, Json, Vertex, VertexProperties, VertexProperty};
+use serde_json::{Number, Value};
 
 #[cfg(test)]
 mod test {
@@ -27,9 +8,9 @@ mod test {
     use super::*;
 
     #[test]
-    fn create_propertied_vertex_from_action() {
-        let test_action = Action::default();
+    fn create_action_vertex() {
+        let action_vertex = Vertex::new(Identifier::new("Action").unwrap());
 
-        let converted_propertied_vertex: VertexProperties = VertexProperties::from(test_action);
+        assert!(action_vertex.t == Identifier::new("Action").unwrap());
     }
 }
