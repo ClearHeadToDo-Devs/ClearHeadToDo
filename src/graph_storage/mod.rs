08ec35c80d087f827e763d54ba75ee3c4c3662ed
Darrion Burgess <dargondab9@gmail.com>
2023-02-05 07:42:47 -0800

    got the first few files turned on for my project view

--- src/graph_storage/mod.rs
+++ src/graph_storage/mod.rs
@@ -1,15 +1,28 @@
-use crate::action::Action;
-use indradb::{VertexProperty, VertexProperties, Vertex, Identifier};
+use crate::{action::Action, action_interface::ActionViewing, priority::Priority};
+use indradb::{Identifier, Vertex, VertexProperties, VertexProperty, Json};
+use serde_json::{Value, Number};
 
-impl From<Action> for VertexProperties{
+impl From<Action> for VertexProperties {
     fn from(value: Action) -> VertexProperties {
         let vertex = Vertex::new(Identifier::new("Action".to_string()).unwrap());
-        let properties = vec![];
+        let vertex_id = vertex.id.clone();
+
+        let properties = vec![
+            VertexProperty::new(vertex_id, Value::String(value.get_name().to_string())),
+            VertexProperty::new(vertex_id, Value::Bool(value.get_completion_status())),
+            VertexProperty::new(vertex_id, Value::Number(value.get_priority().into()))
+        ];
+    }
+}
+
+impl From<&Priority> for Value {
+    fn from(val: &Priority) -> Self {
+        Value::Number(1)
     }
 }
 
 #[cfg(test)]
-mod test{
+mod test {
 
     use super::*;
 
