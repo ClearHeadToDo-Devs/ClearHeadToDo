ca645ec58dd224980a5ab2c59edaa20bd74a64a4
Darrion Burgess <dargondab9@gmail.com>
2023-08-06 18:37:56 -0700

    refactor to reduce test redundancy

--- src/relationship.rs
+++ src/relationship.rs
@@ -1,9 +1,9 @@
 use uuid::Uuid;
 
-use indradb;
 use strum_macros::*;
 
 #[derive(Debug, Clone)]
+
 pub struct Relationship {
     id: Uuid,
     variant: RelationshipVariant,
@@ -43,17 +43,6 @@ impl Default for RelationshipVariant {
     }
 }
 
-impl From<indradb::Identifier> for RelationshipVariant {
-    fn from(identifier: indradb::Identifier) -> Self {
-        match identifier.as_str() {
-            "Parental" => Self::Parental,
-            "Sequential" => Self::Sequential,
-            "Related" => Self::Related,
-            _ => Self::Related,
-        }
-    }
-}
-
 
 #[cfg(test)]
 mod test {
@@ -65,6 +54,9 @@ mod test {
     fn empty_relationship() -> Relationship {
         Relationship::new(Uuid::nil(), None, Uuid::nil(), Uuid::nil())
 }
+    fn create_empty_relationship(variant:  RelationshipVariant)-> Relationship {
+        Relationship::new(Uuid::nil(), Option::Some(variant), Uuid::nil(), Uuid::nil())
+    }
 
     #[rstest]
     fn create_minimal_relationship(empty_relationship: Relationship) {
@@ -75,105 +67,17 @@ mod test {
     }
 
 
-    #[rstest]
+    #[test]
     fn create_parental(){
-        let relationship = Relationship::new(
-            Uuid::nil(),
-            Some(RelationshipVariant::Parental),
-            Uuid::nil(),
-            Uuid::nil(),
-        );
+        let relationship = create_empty_relationship(RelationshipVariant::Parental);
 
         assert!(relationship.variant as usize == 1)
     }
 
     #[test]
     fn create_sequential() {
-        let relationship = Relationship::new(
-            Uuid::nil(),
-            Some(RelationshipVariant::Sequential),
-            Uuid::nil(),
-            Uuid::nil(),
-        );
-
+        let relationship = create_empty_relationship(RelationshipVariant::Sequential);
         assert!(relationship.variant as usize == 2)
-    }
-
-    #[test]
-    fn create_related() {
-        let relationship = Relationship::new(
-            Uuid::nil(),
-            Some(RelationshipVariant::Related),
-            Uuid::nil(),
-            Uuid::nil(),
-        );
-
-        assert!(relationship.variant as usize == 3)
-    }
-
-
-    #[test]
-    fn create_parental_variant() {
-        let relationship_type = RelationshipVariant::Parental;
-
-        assert!(relationship_type as usize == 1)
-    }
-
-    #[test]
-    fn create_sequential_variant() {
-        let sequential = RelationshipVariant::Sequential;
-
-        assert!(sequential as usize == 2)
-    }
-
-    #[test]
-    fn create_related_variant() {
-        let related_type = RelationshipVariant::Related;
-
-        assert!(related_type as usize == 3)
-    }
-
-    #[test]
-    fn create_default_variant() {
-        let default_type = RelationshipVariant::default();
-
-        assert!(default_type as usize == 3)
-    }
-
-    #[test]
-    fn create_parental_variant_from_identifier() {
-        let test_identifier = indradb::Identifier::new("Parental").unwrap();
-
-        let converted_relationship_variant = RelationshipVariant::from(test_identifier);
-
-        assert!(converted_relationship_variant as usize == 1)
-    }
-
-    #[test]
-    fn create_sequential_variant_from_identifier() {
-        let test_identifier = indradb::Identifier::new("Sequential").unwrap();
-
-        let converted_relationship_variant = RelationshipVariant::from(test_identifier);
-
-        assert!(converted_relationship_variant as usize == 2)
-    }
-
-    #[test]
-    fn create_related_variant_from_identifier() {
-        let test_identifier = indradb::Identifier::new("Related").unwrap();
-
-        let converted_relationship_variant = RelationshipVariant::from(test_identifier);
-
-        assert!(converted_relationship_variant as usize == 3)
-    }
-
-    #[test]
-    fn create_related_from_other_variant() {
-        let test_identifier = indradb::Identifier::new("Other").unwrap();
-
-        let converted_relationship_variant = RelationshipVariant::from(test_identifier);
-
-        assert!(converted_relationship_variant as usize == 3)
     }
 }
 
